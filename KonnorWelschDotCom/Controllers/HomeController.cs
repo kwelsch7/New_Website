aab431dcb67156484df0ab10405dcca919bf628c
Konnor Welsch <konnor.welsch@eagles.ewu.edu>
2017-05-11 20:57:57 -0700

    Initial setup of project and site layout

---
+++ KonnorWelschDotCom/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KonnorWelschDotCom.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Home()
+        {
+            return View();
+        }
+
+        public IActionResult Portfolio()
+        {
+            return View();
+        }
+        
+        public IActionResult Projects()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
